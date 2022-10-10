7bb60feeb2519eab52f3db8d107996735a3b5811
Pavithra Subramanian <72555685+pavithra-064@users.noreply.github.com>
2022-10-10 19:05:17 +0530

    [idea] Added BMI calculator (#209)
    * added bmi calculator
    
    * added bmi calculator
    
    * added bmi calculator
    
    * Delete vue.config.js
    
    * Delete .browserslistrc
    
    * removed package-lock.json

---
+++ vue/bmi-calculator/src/App.vue
@@ -0,0 +1,40 @@
+<script>
+import BmiCalculator from "@/components/BmiCalculator";
+import Header from "@/components/Header";
+
+export default {
+  name: "App",
+  components: {
+    BmiCalculator,
+    Header,
+  },
+};
+</script>
+
+<template>
+  <div id="app">
+    <Header></Header>
+    <bmi-calculator></bmi-calculator>
+  </div>
+</template>
+
+<style>
+body {
+  background: url(https://media.istockphoto.com/photos/healthy-fresh-vegetables-frame-copy-space-picture-id1249223936?b=1&k=20&m=1249223936&s=170667a&w=0&h=hxKD4SC8LPwZoIDkyMo894U2qxZl7WAVbJm3Zcme-8M=)
+    no-repeat center fixed;
+  background-size: cover;
+  height: 100%;
+}
+
+#app {
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #ffffff;
+}
+
+html,
+* {
+  margin: 0 0;
+}
+</style>
---
+++ vue/bmi-calculator/src/components/Header.vue
@@ -0,0 +1,29 @@
+<script>
+export default {
+  name: "Header",
+};
+</script>
+
+<template>
+  <section>
+    <div class="header">
+      <h1>BMI CALCULATOR</h1>
+    </div>
+  </section>
+</template>
+
+<style scoped>
+.header {
+  margin-top: 0;
+  top: 0;
+  left: 0;
+  right: 0;
+  background-color: rgba(0, 0, 0, 0.621);
+  height: 15vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  font-size: x-large;
+  margin-bottom: 5vh;
+}
+</style>
